b1f330a148fbc29adfd01792693ba717f9bc34c0
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2024-05-16 10:22:28 +0300

    feat/add-swagger-documentation-for-matches-endpoints

--- src/controllers/matches-controller.js
+++ src/controllers/matches-controller.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { getAllMatches } = require('../services/match-service');
+const { getAllMatches, getOneMatch } = require('../services/match-service');
 const authenticationMiddleware = require('../middlewares/auth-middleware');
 
 router.get('/', authenticationMiddleware, async (req, res, next) => {
   try {
     const allMatches = await getAllMatches();
     res.status(201).json(allMatches);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:id', authenticationMiddleware, async (req, res, next) => {
+  try {
+    const singleMatch = await getOneMatch(req.params.id);
+    res.status(201).json(singleMatch);
   } catch (error) {
     next(error);
   }
