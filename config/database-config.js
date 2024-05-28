3b899d7e5e9ff61a958e34e15d7d2596b31a1c4a
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2024-05-28 21:39:59 +0300

    feat/host-express-api-at-vercel

--- config/database-config.js
+++ config/database-config.js
@@ -1,15 +1,7 @@
 const { Sequelize } = require('sequelize');
 const { readEnvironmentFile } = require('./envFile');
 
-// const sequelize = new Sequelize(
-//   'postgresql://app:94M39oMpou5P4x9260SxAlCD@quietly-cunning-stingray.a1.pgedge.io/scoresaga?sslmode=no-verify',
-//   {
-//     // Choose one of the logging options
-//     logging: (...msg) => console.log(msg), // Displays all log function call parameters
-//   },
-// );
 readEnvironmentFile();
-
 const sequelize = new Sequelize({
   dialect: process.env.DIALECT,
   host: process.env.HOST,
@@ -17,10 +9,10 @@ const sequelize = new Sequelize({
   username: process.env.DB_USERNAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
+  ssl: true,
 });
 
 const connectDB = async () => {
-  console.log('Connecting to the database...');
   try {
     await sequelize.authenticate();
     console.log('Connected to PostgreSQL sucessfully!');
