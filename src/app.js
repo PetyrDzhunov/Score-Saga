3da91a525a7979f6a489e51af6ea9398a0ceaaa9
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2024-05-29 20:00:19 +0300

    fix/disable-cors-and-fix-swagger-css

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const { readEnvironmentFile } = require('../config/envFile.js');
 const getFixturesCronJob = require('./jobs/get_fixtures_cron_job.js');
 const calculateSuccessRateCronJob = require('./jobs/calculate_success_rate_cron_job.js');
 const { swaggerUi } = require('../config/swagger-config.js');
-
+const cors = require('cors');
 const path = require('path');
 const YAML = require('yamljs');
 const swaggerFilePath = path.resolve(__dirname, './swagger.yaml');
@@ -18,11 +18,18 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 app.use(routes);
 app.use(errorHandler);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    customCss:
+      '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+  }),
+);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server started on port ${process.env.PORT || 5000}`);
